e6d2a6a8228f3deef03bec190ade92feb54ee777
Guilherme Delgado Martins <guilhermedelgado876@gmail.com>
2025-05-09 12:40:04 -0300

    feat: melhorar pagina de galeria

--- src/app/pages/galeria/galeria.component.ts
+++ src/app/pages/galeria/galeria.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-galeria',
@@ -6,6 +6,38 @@ import { Component } from '@angular/core';
   templateUrl: './galeria.component.html',
   styleUrl: './galeria.component.scss'
 })
-export class GaleriaComponent {
+export class GaleriaComponent implements OnInit {
+
+  galerias: any[] = [
+    {
+      id: 1,
+      title: 'Galeria 1',
+      image: 'teste.jpg'
+    },
+    {
+      id: 2,
+      title: 'Galeria 2',
+      image: 'teste.jpg'
+    },
+    {
+      id: 3,
+      title: 'Galeria 3',
+      image: 'teste.jpg'
+    },
+  ]
+
+  ngOnInit(): void {
+    const element = document.querySelector('.content') as HTMLElement;
+    if (element) {
+      const rect = element.getBoundingClientRect();
+      var quantidade = Math.round(rect.width / (170 + 16));
+      if(quantidade < this.galerias.length) {
+        for(let i = 0; i < quantidade-1; i++) {
+          this.galerias.push({});
+        }
+      }
+    }
+
+  }
 
 }
