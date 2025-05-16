a36ab8b22bd1a9b4cb850302ef44f2282c933c9d
Guilherme Delgado Martins <guilhermedelgado876@gmail.com>
2025-05-16 12:48:49 -0300

    feat: inclementar grande parte do design de formulario e do whatsapp button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgClass],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'CampManager';
 
-  constructor() {}
+  constructor(public router: Router) {}
+
+  isFormularioRoute(): boolean {
+    return this.router.url.startsWith('/formulario');
+  }
 }
