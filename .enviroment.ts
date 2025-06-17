81666a8a97462128f08e292c92a3b288c8d4121b
Guilherme Delgado Martins <2311600261@fema.edu.br>
2025-06-17 10:36:18 -0300

    feat: mexen

--- .enviroment.ts
+++ .enviroment.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   API: "http://localhost:8080",
   API_REPORTS: "http://localhost:8000",
-  CLIENT: "http://localhost:37173",
+  CLIENT: "http://localhost:39115",
 };
